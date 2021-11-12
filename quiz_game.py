9837a00c2a36875ea1646a86b806f89c5eee0048
Sean Fitzpatrick <sean.fitzpatrick100@icloud.com>
2021-11-12 16:17:36 -0500

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,68 @@
+print("Welcome to my History Quiz")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes": 
+    quit()
+
+print("Okay, Let's Start")
+
+answer = input("How do historians refer to the time period in the 1730s and 1740s when colonists began embracing secular rationalism over religion? ")
+if answer == "The Great Awakening":
+    print("Correct!")
+else:
+    print("Incorrect! The correct answer is The Great Awakening.")
+
+answer = input("What Act were colonists protesting with the Boston Tea Party? ")
+if answer == "The Stamp Act":
+    print("Correct!")
+else:
+    print("Incorrect! The Correct Answer is The Stamp Act.")
+
+answer = input("What city was the first capitol of the United States? ")
+if answer == "New York City":
+    print("Correct!")
+else:
+    print("Incorrect! The correct answer is New York City.")
+
+answer = input("Who was the first president to live in the White House? ")
+if answer == "John Adams":
+    print("Correct!")
+else:
+    print("Incorrect! The Correct Answer is John Adams.")
+
+answer = input("Who was the first president to declare war? ")
+if answer == "James Madison":
+    print("Correct!")
+else:
+    print("Incorrect! The Correct Answer is James Madison.")
+
+answer = input("What was the war that James Madison declared? ")
+if answer == "The War of 1812":
+    print("Correct!")
+else:
+    print("Incorrect! The correct answer is The War of 1812.")
+
+answer = input("Who was the first President to be Impeached? ")
+if answer == "Andrew Johnson":
+    print("Correct!")
+else:
+    print("Incorrect! The correct answer is Andrew Johnson.")
+
+answer = input("About 4,000 members of which Native American tribe died of cold, hunger or disease on the Trail of Tears? ")
+if answer == "Cherokee":
+    print("Correct!")
+else:
+    print("Incorrect! The correct answer is Cherokee.")
+
+answer = input("What was the bloodiest single-day battle of American history? ")
+if answer == "The Battle of Antietam":
+    print("Correct!")
+else:
+    print("Incorrect! The answer is The Battle of Antietam.")
+
+answer = input("Who was the Confederate president during the Civil War? ")
+if answer == "Jefferson Davis":
+    print("Correct!")
+else:
+    print("Incorrect! The correct answer is Jefferson Davis.")
